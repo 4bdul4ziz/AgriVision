dc819c0b60500b51e67f9d52011af7cd430a5a36
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2023-04-05 05:25:59 +0530

    add: script to estimate area

--- agri.py
+++ agri.py
@@ -1,32 +1,42 @@
-#use open cv to extract green areas on a satellite image
-
 import cv2
 import numpy as np 
-import matplotlib.pyplot as plt
+import os
 
-img = cv2.imread('st.png')
+# Define the path to the folder where the screenshots are moved
+folder_path = os.path.expanduser("~/Documents/Screenshots")
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# Loop through all files in the folder
+for filename in os.listdir(folder_path):
+    # Check if the file is a PNG image
+    if filename.endswith(".png"):
+        # Read the image
+        img = cv2.imread(os.path.join(folder_path, filename))
 
-lower_green = np.array([40,50,50])
-upper_green = np.array([80,255,255])
+        # Convert to HSV color space
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-mask = cv2.inRange(hsv, lower_green, upper_green)
+        # Define the lower and upper bounds of the green color
+        lower_green = np.array([40,50,50])
+        upper_green = np.array([80,255,255])
 
-res = cv2.bitwise_and(img,img, mask= mask)
+        # Create a mask to select only the green areas
+        mask = cv2.inRange(hsv, lower_green, upper_green)
 
-cv2.imshow('image',img)
-cv2.imshow('mask',mask)
-cv2.imshow('res',res)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        # Apply the mask to the original image
+        res = cv2.bitwise_and(img, img, mask=mask)
 
+        # Calculate the green area in acres
+        area = 130 * 130 * 0.000247105 * np.sum(mask == 255) / 10000
 
-area = 130 * 130 * 0.000247105 * np.sum(mask == 255) / 10000 #in acres
-print(area)
+        # Display the original image, mask, and result image
+        cv2.imshow('image', img)
+        cv2.imshow('mask', mask)
+        cv2.imshow('res', res)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-
-
+        # Display the calculated green area
+        print(f"The green area in {filename} is {area} acres.")
 
 
 
