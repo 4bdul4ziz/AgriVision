fa507ac1333df5ba9e66b15a28d1d49b66ec1709
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2023-04-05 05:27:00 +0530

    add: script to migrate screen grabs from maps

---
+++ migrate.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+# Define the source and destination paths
+desktop_path = os.path.expanduser("~/Desktop")
+destination_path = os.path.expanduser("/Users/abdul/Desktop/Programming/AgriVision/images")
+
+# Create the destination folder if it doesn't exist
+if not os.path.exists(destination_path):
+    os.makedirs(destination_path)
+
+# Loop through all files on the desktop
+for filename in os.listdir(desktop_path):
+    if filename.startswith("Screenshot"):
+        # Move the file to the destination folder
+        shutil.move(os.path.join(desktop_path, filename), os.path.join(destination_path, filename))
+        print(f"Moved {filename} to {destination_path}")
