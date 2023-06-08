4ea5e4942b4504067d6a0832c086ef11a81dde0f
Aman Gupta <71923741+metal0bird@users.noreply.github.com>
2023-06-08 18:38:16 +0530

    weather attributes api

---
+++ weather_api.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import time
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+
+
+configuration = swagger_client.Configuration()
+configuration.api_key['key'] = 'your api key'
+
+
+
+api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
+q = 'VIT Chennai' 
+dt = '2023-07-06' 
+
+try:
+    # Astronomy API
+    api_response = api_instance.astronomy(q, dt)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling APIsApi->astronomy: %s\n" % e)
+
+
+configuration = swagger_client.Configuration()
+configuration.api_key['key'] = 'your api key'
+
+
+
+api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
+q = 'VIT Chennai'
+days = 56 
+dt = '2013-10-20' 
+unixdt = 56 
+hour = 56 
+lang = 'lang_example' 
+
+try:
+    # Forecast API
+    api_response = api_instance.forecast_weather(q, days, dt=dt, unixdt=unixdt, hour=hour, lang=lang)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling APIsApi->forecast_weather: %s\n" % e)
+
+
+configuration = swagger_client.Configuration()
+configuration.api_key['key'] = 'your api key'
+
+
+
+api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
+q = 'VIT Chennai' 
+dt = '2023-08-08' 
+lang = 'lang_example' 
+
+try:
+    # Future API
+    api_response = api_instance.future_weather(q, dt=dt, lang=lang)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling APIsApi->future_weather: %s\n" % e)
+
+
+
+
+configuration = swagger_client.Configuration()
+configuration.api_key['key'] = 'your api key'
+
+
+
+api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
+q = 'vit chennai'
+lang = 'lang_example' 
+try:
+    # Realtime API
+    api_response = api_instance.realtime_weather(q, lang=lang)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling APIsApi->realtime_weather: %s\n" % e)
+
